626beac5c4fcf036410cf4c1cf60ae3da15f6044
Ayarn <iamayarn2001@gmail.com>
2023-12-18 10:16:21 +0530

    change cors

--- server.js
+++ server.js
@@ -11,19 +11,25 @@ const cookieParser = require("cookie-parser");
 // cors
 const cors = require("cors");
 
-const whitelist = [
-  "https://quote-ocean-frontend-fl54oygiu-ayarn-modis-projects.vercel.app",
-  "https://quote-ocean-frontend.vercel.app",
-];
+// const whitelist = [
+//   "https://quote-ocean-frontend-fl54oygiu-ayarn-modis-projects.vercel.app",
+//   "https://quote-ocean-frontend.vercel.app",
+// ];
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error());
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// };
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error());
-    }
-  },
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
