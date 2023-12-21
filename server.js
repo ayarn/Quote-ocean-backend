424e7cb1c24002a53ae6d6ffedb51e3c92857c5a
Ayarn <iamayarn2001@gmail.com>
2023-12-21 14:31:29 +0530

    server change

--- server.js
+++ server.js
@@ -11,30 +11,30 @@ const cookieParser = require("cookie-parser");
 // cors
 const cors = require("cors");
 
-// const whitelist = [
-//   "https://quote-ocean-frontend-fl54oygiu-ayarn-modis-projects.vercel.app",
-//   "https://quote-ocean-frontend.vercel.app",
-// ];
+const allowedOrigins = [
+  "https://quote-ocean-frontend.vercel.app",
+  /^https:\/\/quote-ocean-frontend-\w+-ayarn-modis-projects\.vercel\.app$/,
+];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error());
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true,
-// };
+// Use dynamic origin based on the request
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Check if the origin is allowed
+      const isAllowed = allowedOrigins.some((allowedOrigin) => {
+        if (typeof allowedOrigin === "string") {
+          return allowedOrigin === origin;
+        } else if (allowedOrigin instanceof RegExp) {
+          return allowedOrigin.test(origin);
+        }
+        return false;
+      });
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+      callback(null, isAllowed ? origin : false);
+    },
+    credentials: true,
+  })
+);
 
 // connection to database
 const connectDB = require("./db/dbConnection");
